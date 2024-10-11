80d415d912b24d169508980caffc5e7bb7aec3a2
fulik0z <fulikozzz@gmail.com>
2024-10-11 23:08:13 +0700

    Добавьте файлы проекта.

---
+++ TANK1990(lab2)/TANK1990(lab2).cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+// Танк игрока
+struct PlayerTank {
+    int pos_x;
+    int pos_y;
+    int vector;
+    int speed;
+    int life_count;
+};
+
+// Танк противника
+struct PlayerTank {
+    int pos_x;
+    int pos_y;
+    int vector;
+    int speed;
+    int life_count;
+};
+
+// Снаряд
+struct Bullet {
+    int pos_x;
+    int pos_y;
+    int vector;
+    int speed;
+    bool isActive;
+};
+
+// Игровое поле
+struct PlayField {
+    int size_x;
+    int size_y;
+};
+
+int main()
+{
+
+}
