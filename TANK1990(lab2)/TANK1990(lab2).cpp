16d0a48b9516a61b067321250266ad2dcc2ad9ba
Ilya <kazancevila68@gmail.com>
2024-10-11 23:55:14 +0700

    Создание движений снаряда

--- TANK1990(lab2)/TANK1990(lab2).cpp
+++ TANK1990(lab2)/TANK1990(lab2).cpp
@@ -35,6 +35,33 @@ struct PlayField {
     int size_y;
     int** grid;
 };
+// Функция для стрельбы
+void shootBullet(struct Bullet* bullet, struct PlayerTank* tank) {
+    if (!bullet->isActive) {
+        bullet->pos_x = tank->pos_x;
+        bullet->pos_y = tank->pos_y;
+        bullet->vector = tank->vector;
+        bullet->speed = 1;
+        bullet->isActive = true;
+        printf("Снаряд выпущен с позиции (%d, %d)\n", bullet->pos_x, bullet->pos_y);
+    }
+}
+
+// Функция для перемещения пули
+void moveBullet(struct Bullet* bullet) {
+    if (bullet->isActive) {
+        switch (bullet->vector) {
+        case 0: bullet->pos_y -= bullet->speed; break; 
+        case 1: bullet->pos_x += bullet->speed; break; 
+        case 2: bullet->pos_y += bullet->speed; break; 
+        case 3: bullet->pos_x -= bullet->speed; break; 
+        }
+        printf("Координаты снаряда (%d, %d)\n", bullet->pos_x, bullet->pos_y);
+
+       
+    }
+}
+
 
 // Инициализация игрового поля
 void initPlayField(struct PlayField* field, int size_x, int size_y) {
